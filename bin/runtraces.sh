31a103265d8b80f1307a67da5af9955139ecca1f
Aneesh Neelam <neelam.aneesh@gmail.com>
2016-05-29 12:13:04 -0700

    Traces script
    * Hide heartbeat
    * Helper statements printed

--- bin/runtraces.sh
+++ bin/runtraces.sh
@@ -2,10 +2,12 @@
 
 echo "VLDP AMPM Hybrid\n"
 for f in ./traces/*.dpc.gz; do
-    zcat "$f" | ./bin/vldp_ampm
+    echo "Running trace: $f on VLDP AMPM Hybrid"
+    zcat "$f" | ./bin/vldp_ampm -hide_heartbeat
 done
 
 echo "DCPT VLDP Hybrid\n"
 for f in ./traces/*.dpc.gz; do
-    zcat "$f" | ./bin/dcpt_vldp
+    echo "Running trace: $f on DCPT VLDP Hybrid"
+    zcat "$f" | ./bin/dcpt_vldp -hide_heartbeat
 done
